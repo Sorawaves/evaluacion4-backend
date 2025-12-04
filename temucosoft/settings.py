3235d33e153e5918d2f461124b4783a0f8d6b4d6
Sorawaves <mmrblasi@gmail.com>
2025-12-04 04:07:58 -0300

    Add dotenv loading for environment variables

--- temucosoft/settings.py
+++ temucosoft/settings.py
@@ -14,6 +14,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
